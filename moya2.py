8f1ffbf506b8627593e2b99d462ab21a13015848
rezqeyyy <rizqiasanmm@gmail.com>
2025-01-03 11:20:23 +0700

    new

--- moya2.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-# Ganti dengan API Key Anda
-api_key = "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"
-encoded_key = base64.b64encode(api_key.encode("utf-8")).decode("utf-8")
-
-print(encoded_key)
